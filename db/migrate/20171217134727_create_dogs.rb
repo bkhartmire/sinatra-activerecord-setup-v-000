dde196761ab0f1859f6c1fe61ff7f630513e6c68
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-17 13:49:17 +0000

    automatically backed up by learn

--- db/migrate/20171217134727_create_dogs.rb
+++ db/migrate/20171217134727_create_dogs.rb
@@ -1,12 +1,8 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
+  def change
     create_table :dogs do |dog|
       dog.string :name
       dog.string :breed
     end
-  end
-
-  def down
-    drop_table :dogs
-  end
+  
 end
